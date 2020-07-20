fb08b5c1ae932827a4a57ce14b27e54c1b11f1f3
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-07-20 11:22:27 +0200

    publish

---
+++ index.js
@@ -0,0 +1,15 @@
+function randomNumber(min,max){
+
+    if (typeof(max) !== 'number' && typeof(min) !== 'number'){
+
+        min = 0;
+        max = 1;
+
+    }
+
+    return (Math.random() * (max-min) + min)
+
+
+}
+
+module.exports = randomNumber;
